ffa3e0db80db634307e4a265db72f1e646829f9b
Hill-Man-1 <hilmaaansf@gmail.com>
2024-05-19 12:32:26 +0700

    feat:get teknisi desc by order handle

--- src/controller/teknisiController.js
+++ src/controller/teknisiController.js
@@ -1,4 +1,4 @@
-import { getAllTeknisiService } from "../service/teknisiService.js";
+import { getAllTeknisiService, getTeknisiDescService } from "../service/teknisiService.js";
 
 
 const getAllTeknisi = async (req, res, next) => {
@@ -14,4 +14,17 @@ const getAllTeknisi = async (req, res, next) => {
     }
 }
 
-export { getAllTeknisi}
+const getTeknisiDesc = async (req, res, next) => {
+    try {
+        const teknisiList = await getTeknisiDescService();
+        res.status(200).json({
+            code: "0",
+            info: "OK",
+            data: teknisiList
+        });
+    } catch (err) {
+        next(err);
+    }
+}
+
+export { getAllTeknisi,getTeknisiDesc}
--- src/routes/v1/teknisi/index.js
+++ src/routes/v1/teknisi/index.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import authenticationMiddleware from '../../../middleware/auth.js';
 import {authorizationAdmin, authorizationCustomer, authorizationTeknisi} from '../../../middleware/authorize.js'
-import { getAllTeknisi } from '../../../controller/teknisiController.js';
+import { getAllTeknisi, getTeknisiDesc } from '../../../controller/teknisiController.js';
 
 
 const teknisiRoute = express.Router();
 
 
 teknisiRoute.get('/list',authenticationMiddleware ,authorizationAdmin ,getAllTeknisi)
-// teknisiRoute.get('/get-desc', limitMiddleware ,getAllTeknisi)
+teknisiRoute.get('/list-desc', authenticationMiddleware,authorizationAdmin ,getTeknisiDesc)
 
 export default teknisiRoute
